df3b4a6134786d08a4fb0f56027d6c4e59494f6b
Pablo-PC <pcassettai@gmail.com>
2019-09-25 20:14:40 -0300

    incorporación de try except

--- AREXTI_DJANGO/AREXTI_PROCESOS/ProcesarDirectorio.py
+++ AREXTI_DJANGO/AREXTI_PROCESOS/ProcesarDirectorio.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import imghdr
 import datetime
 import time
@@ -55,34 +56,50 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                 ImagenesCola.put([dirName, fname, ext])
 
 
-''' 
-Funcion que realiza todo el proceso
-'''
-
-
+# Funcion que procesa la imagen recibida por paramentro
+# noinspection PyBroadException
 def procesar_imagen(procesoid, imagenes_cola, imagenes_guardar, imagenes_notexto):
     # instancio las RN
-    rn_txt = RedesNeuronales.RedNeuronalTexto()
-    rn_chat = RedesNeuronales.RedNeuronalChat()
-    #rn_mail = RedesNeuronales.RedNeuronalEmail()
+    try:
+        rn_txt = RedesNeuronales.RedNeuronalTexto()
+    except:
+        logging.error('Error al instanciar la RN de Texto - ' + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+
+    try:
+        rn_chat = RedesNeuronales.RedNeuronalChat()
+    except:
+        logging.error('Error al instanciar la RN de Chat - ' + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+
+    # try:
+    #     # rn_mail = RedesNeuronales.RedNeuronalEmail()
+    # except:
+    #     logging.error('Error al instanciar la RN de Email - ' + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
 
     # instancio el segmentador
-    segmentador = Segmentacion.Segmentador(720)
+    try:
+        segmentador = Segmentacion.Segmentador(720)
+    except:
+        logging.error('Error al instanciar el segmentador - ' + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
 
     while not imagenes_cola.empty():
         img_procesar = imagenes_cola.get()
         img_path = img_procesar[0]
         img_nombre = img_procesar[1]
 
-        #print("--- Proceso {0} - imagen {1} ---".format(procesoid, img_nombre))
+        # print("--- Proceso {0} - imagen {1} ---".format(procesoid, img_nombre))
         imagen_procesada = ImagenProcesar.Imagen()
         imagen_procesada.set_nombre(img_procesar[1])
         imagen_procesada.set_extension(img_procesar[2])
         imagen_procesada.set_path(img_procesar[0])
 
         # Verifica si posee texto o no con la RN
-        img_path = img_path + os.sep
-        tiene_texto = rn_txt.imagen_tiene_texto(img_path, img_nombre)
+        try:
+            img_path = img_path + os.sep
+            tiene_texto = rn_txt.imagen_tiene_texto(img_path, img_nombre)
+        except:
+            msgerror = 'Error en RN Texto, al intentar predecir la imagen (' + imagen_with_path + ") "
+            logging.error(msgerror + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+            continue
 
         if not tiene_texto:  # Si la imagen NO posee texto
             imagenes_notexto.put(img_path+img_nombre)
@@ -91,23 +108,38 @@ def procesar_imagen(procesoid, imagenes_cola, imagenes_guardar, imagenes_notexto
             imagen_with_path = img_path + img_nombre
 
             # Calcula Hashes
-            listado_hashes = {"md5": "", "sha1": "", "sha256": ""}
-            listado_hashes = Hashes.calcular_hashes(listado_hashes, imagen_with_path)
-            imagen_procesada.set_hashes(listado_hashes)
+            try:
+                listado_hashes = {"md5": "", "sha1": "", "sha256": ""}
+                listado_hashes = Hashes.calcular_hashes(listado_hashes, imagen_with_path)
+                imagen_procesada.set_hashes(listado_hashes)
+            except:
+                msgerror = 'Error al intentar calcular los hash de la imagen (' + imagen_with_path + ") "
+                logging.error(msgerror + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+                continue
 
             # Extrae metadatos
-            listado_metadatos = Metadatos.metadata_extraer(imagen_with_path)
-            imagen_procesada.set_metadatos(listado_metadatos)
+            try:
+                listado_metadatos = Metadatos.metadata_extraer(imagen_with_path)
+                imagen_procesada.set_metadatos(listado_metadatos)
+            except:
+                msgerror = 'Error al intentar calcular los metadatos de la imagen ('+imagen_with_path+") "
+                logging.error(msgerror + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+                continue
 
             # Verifica si es de chat o no con la RN
-            img_path = img_path + os.sep
-            es_chat = rn_chat.imagen_es_chat(img_path, img_nombre)
+            try:
+                img_path = img_path + os.sep
+                es_chat = rn_chat.imagen_es_chat(img_path, img_nombre)
+            except:
+                msgerror = 'Error en RN Chat, al intentar predecir la imagen (' + imagen_with_path + ") "
+                logging.error(msgerror + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+                continue
 
             if es_chat:
                 imagen_procesada.set_imagentipo("C")
                 # Segmenta la imagen y extraer texto DE CHAT
             else:
-                imagen_procesada.set_imagentipo("O") # no chat
+                imagen_procesada.set_imagentipo("O")  # no chat
                 '''
                 # Verifica si es de mail o no con la RN
                 
@@ -134,7 +166,6 @@ def procesar_imagen(procesoid, imagenes_cola, imagenes_guardar, imagenes_notexto
             imagenes_guardar.put(imagen_segmentada)
 
 
-
 '''
  Inicio del procesamiento en paralelo
  
@@ -154,7 +185,7 @@ if __name__ == '__main__':
     print("Inicio la RN")
     TiempoInicial = datetime.datetime.now()
 
-    procesos_paralelos = 4 #os.cpu_count()  # cantidad de procesos maximos a utilizar
+    procesos_paralelos = os.cpu_count()  # cantidad de procesos maximos a utilizar
     procesos_ejecucion = []              # cantidad de procesos en ejecución
     indiceProceso = 1
 
@@ -162,7 +193,7 @@ if __name__ == '__main__':
     while len(procesos_ejecucion) < procesos_paralelos and not ImagenesCola.empty():
         p = Process(name="Proceso {0}".format(indiceProceso),
                     target=procesar_imagen,
-                    args=(indiceProceso, ImagenesCola, ImagenesGuardar_Cola,imagenesNoTexto,)
+                    args=(indiceProceso, ImagenesCola, ImagenesGuardar_Cola, imagenesNoTexto,)
                     )
         p.start()
         procesos_ejecucion.append(p)
@@ -191,7 +222,7 @@ if __name__ == '__main__':
         # Realizar mas pruebas (si la cola "ImagenesGuardar_Cola" se llena los procesos no terminan)
         while not ImagenesGuardar_Cola.empty():
             img_guardar = ImagenesGuardar_Cola.get()
-            print("Imagen: {0} - {1}".format(img_guardar.get_nombre(),img_guardar.get_imagentipo()))
+            print("Imagen: {0} - {1}".format(img_guardar.get_nombre(), img_guardar.get_imagentipo()))
 
         # Para no saturar el cpu, dormimos el ciclo durante 1 segundo
         time.sleep(1)
