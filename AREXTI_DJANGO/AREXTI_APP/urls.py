b7e44fe6a11e0eee7f192a02a7137e7055e58776
javier <javierangelucci@gmail.com>
2019-11-18 23:22:45 -0300

    Implementa subida de archivos masiva. Ajusta la solapa de archivos en el template ImagenCreate Ajusta los links al volver hacia atras, al editar imagen. Agrega numero de paginacion al listado de Imagen. Ajusta las librerias para crear el treeview. Crea formulario ImagenConsultarForm y UploadFileForm. Ajusta la property disabled = true a los combos y datetime de los formularios. Agrega modelo UploadFile. Crea metodo celery call_ChangeImageType que sera invocado desde la edicion de la imagen.

--- AREXTI_DJANGO/AREXTI_APP/urls.py
+++ AREXTI_DJANGO/AREXTI_APP/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -18,4 +19,6 @@ urlpatterns = [
     path('ReporteOcurrencia/', views.ReporteOcurrencia.as_view(), name='ReporteOcurrencia'),
     path('PericiaConsultar/<int:pk>/<int:Proyectoid>/', views.PericiaConsultar.as_view(), name='PericiaConsultar'),
     path('ProyectoConsultar/<int:pk>/', views.ProyectoConsultar.as_view(), name='ProyectoConsultar'),
+    path('ImagenConsultar/<int:pk>/', views.ImagenConsultar.as_view(), name='ImagenConsultar'),
+    path('BasicUpload/<int:pericia>/', views.BasicUploadView.as_view(), name='BasicUpload'),
 ]
