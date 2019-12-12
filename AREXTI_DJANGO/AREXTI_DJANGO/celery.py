90ab2ec039f296416811fb77cab425d39ec58a8a
Mariano <mcortinez88@gmail.com>
2019-12-12 18:43:50 -0300

    Se bajo de version en celery a 3.1.25. Por error en celery 4.0 en adelante donde se pierde la conexion y se vuelve a reprocesar.

--- AREXTI_DJANGO/AREXTI_DJANGO/celery.py
+++ AREXTI_DJANGO/AREXTI_DJANGO/celery.py
@@ -1,8 +1,11 @@
 import os
 from celery import Celery
+from . import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AREXTI_DJANGO.settings')
 
 app = Celery('AREXTI_DJANGO')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+# app.config_from_object('django.conf:settings', namespace='CELERY') Celery 4.3
+app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks() Celery 4.3
+app.autodiscover_tasks(settings.INSTALLED_APPS)
