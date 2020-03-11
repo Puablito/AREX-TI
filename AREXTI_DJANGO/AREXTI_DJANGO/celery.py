6b7f129f8713b2e9be83523d4ddd68ca378f42d3
Mariano <mcortinez88@gmail.com>
2020-03-10 22:32:43 -0300

    Cambios para usar celery 4.3

--- AREXTI_DJANGO/AREXTI_DJANGO/celery.py
+++ AREXTI_DJANGO/AREXTI_DJANGO/celery.py
@@ -6,6 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AREXTI_DJANGO.settings')
 
 app = Celery('AREXTI_DJANGO')
 # app.config_from_object('django.conf:settings', namespace='CELERY') # Celery 4.3 comentar para 4.4
-# app.config_from_object('django.conf:settings') # Celery 3.1.25
-app.autodiscover_tasks() # Celery 4.3
-# app.autodiscover_tasks(settings.INSTALLED_APPS) # Celery 3.1.25
+app.config_from_object('django.conf:settings') # Celery 3.1.25
+# app.autodiscover_tasks() # Celery 4.3
+app.autodiscover_tasks(settings.INSTALLED_APPS) # Celery 3.1.25
