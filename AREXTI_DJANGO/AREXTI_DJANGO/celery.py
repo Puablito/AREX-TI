b09ffcfdd477a721b7b3478921afee504bb04359
Mariano <mcortinez88@gmail.com>
2020-03-11 22:49:23 -0300

    Ajustes reportes xlsx

--- AREXTI_DJANGO/AREXTI_DJANGO/celery.py
+++ AREXTI_DJANGO/AREXTI_DJANGO/celery.py
@@ -7,7 +7,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AREXTI_DJANGO.settings')
 app = Celery('AREXTI_DJANGO')
 # app.config_from_object('django.conf:settings', namespace='CELERY') # Celery 4.3 comentar para 4.4
 app.config_from_object({"django.conf":settings,
-                        "broker_heartbeat":300, })  # Celery 3.1.25
+                        "broker_heartbeat":0, })  # Celery 3.1.25
 # app.autodiscover_tasks() # Celery 4.3
 app.autodiscover_tasks(settings.INSTALLED_APPS)  # Celery 3.1.25
 
